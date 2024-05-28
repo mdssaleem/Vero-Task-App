9eb728eeb5c6bd807984df8a3d5efc701aeccfcf
Mohd Saleem <Mdsaleem320@gmail.com>
2024-05-28 15:30:16 +0530

    Project base structure setp

---
+++ VeroTaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeroTaskApp
+//
+//  Created by MOHD SALEEM on 28/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
